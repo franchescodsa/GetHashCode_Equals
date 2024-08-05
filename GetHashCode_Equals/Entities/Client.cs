72e2a825d5d09411402c49b5a04288d6d4b7f3a6
franchesco aprigio <franchescodsa@outlook.com>
2024-08-05 10:24:35 -0300

    Adicionar arquivos de projeto.

---
+++ GetHashCode_Equals/Entities/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHashCode_Equals.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Client(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override bool Equals(object? obj)
+
+        {//Foi definida a COMPARAÇÃO ENTRE EMAILs
+            if (!(obj is Client))
+            {
+                return false;
+                
+            }
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ GetHashCode_Equals/Program.cs
@@ -0,0 +1,32 @@
+﻿using GetHashCode_Equals.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace GetHashCode_Equals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*/Equals
+
+            string a = "Maria";
+            string b = "Maria";
+            Console.WriteLine(a.Equals(b));
+
+            //GetHashCode
+
+            string c = "Maria";
+            string d = "Alex";
+            Console.WriteLine(c.GetHashCode());
+            Console.WriteLine(d.GetHashCode());
+            */
+
+            Client a = new Client("Franchesco", "franchescodsa@gmail.com");
+            Client b = new Client("Miguel", "Miguel@gmail.com");
+
+            Console.WriteLine(a.Equals(b));
+
+
+        }
+    }
+}
